1df10168833a921b63a3621b20922bb135d7fa8b
ElineArt <elineart@gmail.com>
2025-03-22 00:04:07 -0400

    adds basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,20 +1,16 @@
 def addition_subtraction():
-  """
-  This function takes two numbers as input from the user,
-  performs addition and subtraction, and displays the results.
-  """
   try:
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = float(input("Please type your first number:  "))
+    num2 = float(input("Please type your second number:"))
 
     addition_result = num1 + num2
     subtraction_result = num1 - num2
 
-    print(f"Addition result: {addition_result}")
-    print(f"Subtraction result: {subtraction_result}")
+    print(f"This is the result for the Addition: {addition_result}")
+    print(f"This is the result for the Subtraction: {subtraction_result}")
 
   except ValueError:
-    print("Invalid input. Please enter valid numbers.")
+    print("This input is not allowed. Try again.")
 
 if __name__ == "__main__":
   addition_subtraction()
