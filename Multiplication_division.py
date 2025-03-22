703040c992c99184018014621a4291321a99e7fd
ElineArt <elineart@gmail.com>
2025-03-22 00:07:27 -0400

    Update Multiplication_division.py

--- Multiplication_division.py
+++ Multiplication_division.py
@@ -1,25 +1,22 @@
 def multiplication_division():
-  """
-  This function takes two numbers as input, performs multiplication and division,
-  and displays the results. It handles division by zero.
-  """
+  
   try:
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = float(input("Please type your first number: "))
+    num2 = float(input("Please type your second number: "))
 
     multiplication_result = num1 * num2
 
     if num2 != 0:
       division_result = num1 / num2
-      print(f"Division result: {division_result}")
+      print(f"The result of the division is: {division_result}")
     else:
-      print("Division by zero is not allowed.")
+      print("It is not allowed to divide  by zero.")
       division_result = "Undefined"
 
-    print(f"Multiplication result: {multiplication_result}")
+    print(f"This is the result for the multiplication: {multiplication_result}")
 
   except ValueError:
-    print("Invalid input. Please enter valid numbers.")
+    print("This is an invalid input. Try again.")
 
 if __name__ == "__main__":
   multiplication_division()
